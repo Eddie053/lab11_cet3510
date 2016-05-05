79786ca012dcfc8e09cad060c00cc81f2414364c
Eddie053 <eddie.chan1@hotmail.com>
2016-05-05 18:29:03 -0400

    first

---
+++ ground_control_win.asm
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+ global _main
+ extern _printf
+
+ section .text
+ 
+ _main:
+       push  message
+       call  _printf
+       add   esp,4
+       ret
+
+ message:
+         db 'Ground Control to Major Eddie', 10,0
